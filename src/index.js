df880366809a3c78bd794537e115f3274840f6e2
ethanshry <ehshry@gmail.com>
2020-10-18 10:59:15 -0500

    basic server ready

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+let app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.get("/route/:name", (req, res) => {
+  console.log(req.params.name);
+  res.send("ping " + req.params.name);
+});
+
+app.listen(7211, () => {
+  console.log("listening on 7211");
+});
